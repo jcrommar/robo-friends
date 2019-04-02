860683fa337f4d9219b7310232379a4686f5cc58
JC Romualdo <juan.romualdo@zoro.com>
2019-04-02 09:47:12 -0500

    Updates

---
+++ precache-manifest.ce3a4d9765c11c7f50de31d28e521dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "4f616a4776732455535c",
+    "url": "/robo-friends/static/js/main.912db8d9.chunk.js"
+  },
+  {
+    "revision": "ab4cb19aa711d405aa65",
+    "url": "/robo-friends/static/js/2.ffabdffb.chunk.js"
+  },
+  {
+    "revision": "4f616a4776732455535c",
+    "url": "/robo-friends/static/css/main.d6a2d8d7.chunk.css"
+  },
+  {
+    "revision": "ab4cb19aa711d405aa65",
+    "url": "/robo-friends/static/css/2.471ca4e9.chunk.css"
+  },
+  {
+    "revision": "ccd1084ed3b58435bec3294594b6448a",
+    "url": "/robo-friends/index.html"
+  }
+];
