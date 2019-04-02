6217ab02426ad4f6596bbb8edfe80ab89e287943
JC Romualdo <juan.romualdo@zoro.com>
2019-04-02 09:01:40 -0500

    new version

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SearchBox = ({ searchfield, searchChange }) => {
+const SearchBox = ({ searchField, searchChange }) => {
     return (
         <div className='pa2'>
             <input
             className='pa3 ba b--green bg-lightest-blue' 
             type='search' 
-            placeholder='Search Robots'
+            placeholder='Search robots'
             onChange={searchChange} 
             />
         </div>
