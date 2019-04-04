5675027e4d35dd98fb6a4309ec256a68cce8dd08
JC Romualdo <juan.romualdo@zoro.com>
2019-04-04 12:01:35 -0500

    splitting of App to unit test

---
+++ src/components/MainPage.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+import './MainPage.css'; 
+
+class MainPage extends Component {
+
+    componentDidMount() {
+        this.props.onRequestRobots();
+    }
+
+    filterRobots = () => {
+        return this.props.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
+        })
+    }
+    render() {
+        const { robots, onSearchChange, isPending } = this.props;
+        return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends.com</h1>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <Scroll>
+                    { isPending ? <h1>Loading</h1> :
+                        <ErrorBoundary>
+                        <CardList robots={this.filterRobots(robots)} />
+                        </ErrorBoundary>
+                    }
+                    </Scroll>
+                </div>
+            );
+        }
+    }
+
+export default MainPage;
